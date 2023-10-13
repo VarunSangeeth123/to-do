34d82934667d42aa30288ad6b3ae83ca440d45d6
varun <varunsangeethmr@gmail.com>
2023-10-13 12:36:53 +0530

    to do list updated to localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
+import Head from "./component/Head";
 
 function App() {
+  const [newitem, setNewItem] = useState("");
+  const [list, addList] = useState(()=>JSON.parse((localStorage.getItem("todos")))|| []);
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(list));
+  },[list])
+
+  function addItem() {
+    let a = [...list];
+    let ob = {
+      name: newitem,
+      check: false,
+    };
+    a.push(ob);
+    addList(a);
+  }
+
+  function delItem(ind) {
+    console.log(ind);
+    let amd = list;
+    amd.splice(ind, 1);
+    addList([...amd]);
+  }
+
+  function check(val, ind) {
+    console.log(val, ind);
+
+    list[ind].check = val;
+
+    addList([...list]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <Head val={"Todo-List"}></Head>
+
+      <input
+      className="textbox"
+        type="text"
+        placeholder="Add List.."
+        value={newitem}
+        onChange={(e) => setNewItem(e.target.value)}
+      ></input>
+
+      <button className="btn" onClick={() => addItem()}>ADD</button>
+
+      <ul className="abc">
+        {list.map((el, i) => (
+          <li key={i}>
+            {!el.check ? el.name : <span className="str">{el.name}</span>}{" "}
+            &nbsp;{" "}
+            <input
+              type="checkbox"
+              onChange={(e) => check(e.target.checked, i)}
+            ></input>{" "}
+            &nbsp;{" "}
+            <span className="link" onClick={() => delItem(i)}>
+              del
+            </span>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/component/Head.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Head(props) {
+  return (
+    <h1>{props.val}</h1>
+  )
+}
+
+export default Head
